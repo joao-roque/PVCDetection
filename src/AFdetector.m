979361bf012970f4bde4ca58a58b1aee5018d411
djesus95 <33521691+djesus95@users.noreply.github.com>
2017-11-22 15:19:51 +0000

    rPeakDetection use and beggining of feature extraction

--- src/AFdetector.m
+++ src/AFdetector.m
@@ -10,41 +10,41 @@ ecgFull = fullProcessing(ecg);
 e5 = ecgFull{5};
 
 %% 
-threshold = 0.7 * mean (e5);
-timeVector = [1/1000: 1/1000: length(e5)/1000]';
 
-% figure
-% plot(timeVector, e5, 'b', timeVector, threshold*ones(size(timeVector)),'r')
+correctedLocs = rPeakDetection(e5,DAT);
 
-[peaks,locs] = findpeaks(e5, 'MinPeakDistance', 300, 'MinPeakHeight', threshold);
+%% RR
 
-peaksValid = [];
-locsValid = [];
+distanceToLast = [];
 
-figure
-plot(1:length(e5), e5, 'b', 1:length(e5), threshold*ones(size(timeVector)),'r')
-hold on
-stem(locs, peaks, 'g', 'LineStyle', 'none')
-hold off
+for i = 2 : length(correctedLocs)
+    distanceToLast = [distanceToLast correctedLocs(i)-correctedLocs(i-1)];
+end
 
-%% qrs detection. back search of 0.5 seconds
+meanWindow = [];
 
-correctedLocs=[];
-for j=1:length(locs)
-    if j>=2        
-        % max of the range of 500 values
-        [maxPeakWindow,loc1] = max(ecg(locs(j)-500:locs(j)));
-        loc2 = 500 - loc1;
-        correctedLocs = [correctedLocs, locs(j)-loc2-1];
+for j = 1 : length(distanceToLast)
+    
+    if j+249<=length(distanceToLast)
+        meanWindow = [meanWindow repmat(mean(distanceToLast(j:j+249)),1,250)];
     else
-        [maxPeakWindow,loc] = max(0:locs(j));
-        correctedLocs = [correctedLocs, loc];
+        meanWindow = [meanWindow repmat(mean(distanceToLast(j:end)),1,length(distanceToLast)-i)];
     end
 end
 
 figure
 plot(1:length(ecg), ecg)
 hold  
-%stem(locs(2:length(locs))-0.171*1000, DAT.ecg(locs(2:length(locs))-0.171*1000),'g','LineStyle','none')
 stem(correctedLocs, ecg(correctedLocs),'g','LineStyle','none' )
-%stem(locs, peaks, 'g', 'LineStyle', 'none')
+plot(class)
+hold off
+
+
+
+figure
+plot(meanWindow)
+% hold 
+% plot(class)
+% ylim([-1,2])
+% hold off
+
